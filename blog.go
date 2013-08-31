2bbcf468515404756abb15de0f232431e8955ea9
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-09-01 02:15:21 +0300

    WIP: command line interface quirks

--- blog.go
+++ blog.go
@@ -16,51 +16,70 @@ import "flag"
 import "fmt"
 import "sort"
 
+
 var COMMANDS = map[string]string{
-    "new": "bla", 
-    "edit": "bla", 
-    "publish": "bla",
-    "list": "bla"}
+    "new": "<post-name> [<params>] - creates new post and opens editor", 
+    "edit": "<post-name>           - opens post in editor", 
+    "publish": "[<post-name>]      - renders markdown posts to html"}
+
 
 func printHeader() {
-    fmt.Println("╔════════════════════════════════╗")
-    fmt.Println("╟          BLOG GENERATOR        ╢")
-    fmt.Println("╚════════════════════════════════╝")
-    fmt.Println("Usage: blog [--use-editor|no-editor] [--help]")
-    fmt.Println("                <command> [<args>]")
-    fmt.Println("")
-    
-
+    fmt.Println("╔════════════════════════════════════════╗")
+    fmt.Println("╟ ░░░░░░░░░░░ BLOG GENERATOR ░░░░░░░░░░░ ╢")
+    fmt.Println("╚════════════════════════════════════════╝")
+    fmt.Println("Usage: blog [--config <cfg-file>] [--help]")
+    fmt.Println("             <command> [<args>]")
+    fmt.Println("──────────────────────────────────────────")
 }
 
-func list_commands() {
-    fmt.Println("Available commands:")
 
+func listCommands(full_description bool) {
+    if (full_description) {
+        fmt.Println("Available commands:")
+    }
+    
     var keys []string
     for k := range COMMANDS {
         keys = append(keys, k)
     }
     sort.Strings(keys)
     for _, command := range keys {
-        fmt.Println("\t", command, " - ", COMMANDS[command])
+        if (full_description) {
+            fmt.Println(" ■", command, COMMANDS[command])
+        } else {
+            // TODO: params
+            fmt.Println(command)
+        }
     }
 }
 
+
+func parseArguments(args []string) {
+    // iterate through params and form a map of --properties, action and action params
+
+}
+
+
 func main() {
     flag.Parse()
+    parseArguments(flag.Args())
+
     var action = flag.Arg(0)
 
-    // print list of completion rules, maybe introduce smth like --silent flag
     if (action == "autocomplete") {
-        list_commands()
+        listCommands(false)
         return 
     }
 
-    printHeader()
-
     switch {
-        case action == "list": list_commands()
-        default: list_commands()
+        case action == "new": fmt.Println("new post")
+        case action == "edit": fmt.Println("edit")
+        case action == "publish": fmt.Println("publish")
+
+        default: {
+            printHeader()
+            listCommands(true)
+        } 
     }
 
 }
