16e86844190d68e73503891b8f5c7490d2303b8f
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-09-10 05:03:25 +0300

    WIP: testing of configuration reading

--- config.go
+++ config.go
@@ -17,42 +17,15 @@ type Config struct {
 	Port string
 }
 
-///////////
-// CRAP |
-////////v///
-
-// TODO: fix this mess
-type AppCfg struct {
-	Foo string
-
-	// foo string - not exported
-	//AparentAddr string
-	//Aaddr string
-	//Aip string        idmap map[string] string
-}
-
-//func NewFile(fd int, name string) *File {
-//	if fd < 0 {
-//		return nil
-//	}
-//	return &File{fd: fd, name: name} // ← Create a new File 
-//}
-
-//func NewConfig()
-
-func HelloFromLib() {
-	fmt.Println("hello from blog lib")
-}
-
-func (l *AppCfg) ReadConfig(path string) (err error) {
+func (cfg *Config) ReadConfig(path string) (err error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println("can't read file "+path, err)
-		return
+		return err
 	}
-	err = json.Unmarshal(b, &l)
+	err = json.Unmarshal(b, &cfg)
 	if err != nil {
-		fmt.Println("error while reading json ", err)
+		fmt.Println("error while unmarshaling json ", err)
 	}
-	return
+	return err
 }
---
+++ config_test.go
@@ -0,0 +1,114 @@
+package blog
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+// if a pointer to a struct is passed, get the type of the dereferenced object
+//
+func toValueType(t reflect.Type) reflect.Type {
+	if t.Kind() == reflect.Ptr {
+		return t.Elem()
+	}
+	return t
+}
+
+func toValue(t reflect.Value) reflect.Value {
+	if t.Kind() == reflect.Ptr {
+		return t.Elem()
+	}
+	return t
+}
+
+// simple structure diff
+// 
+func structDiff(a interface{}, b interface{}) (err error) {
+	var typeA reflect.Type = toValueType(reflect.TypeOf(a))
+	var typeB reflect.Type = toValueType(reflect.TypeOf(b))
+
+	if typeA != typeB {
+		return errors.New(fmt.Sprintf("%v has different type than %v", typeA, typeB))
+	}
+
+	if typeA.Kind() != reflect.Struct {
+		return errors.New(fmt.Sprintf("%v is not reflect.Struct", typeA))
+	}
+
+	structA := toValue(reflect.ValueOf(a))
+	structB := toValue(reflect.ValueOf(b))
+
+	diffs := []string{}
+
+	// loop through the struct's fields and set the map
+	for i := 0; i < typeA.NumField(); i++ {
+		vA := structA.Field(i)
+		vB := structB.Field(i)
+
+		if vA.Interface() != vB.Interface() {
+			fieldType := typeA.Field(i)
+			diffs = append(diffs, fmt.Sprintf("'%v' != '%v', field %v.%v", vA, vB, typeA, fieldType.Name))
+		}
+	}
+	if len(diffs) > 0 {
+		return errors.New(strings.Join(diffs, "\n"))
+	}
+	return nil
+}
+
+// testing the diff machinery
+//
+func TestDiff(t *testing.T) {
+	v1 := "Test"
+	v2 := 1
+	err := structDiff(v1, v2)
+
+	if err == nil {
+		t.Error("diff should work only on same types")
+	}
+
+	err = structDiff(v1, v1)
+	if err == nil {
+		t.Error("diff should work only on structs")
+	}
+
+	c1 := new(Config)
+	err = structDiff(c1, c1)
+	if err != nil {
+		t.Error("diff of two same structs should be empty")
+	}
+
+	c2 := new(Config)
+
+	err = structDiff(c1, c2)
+	if err != nil {
+		t.Error("diff of two same structs should be empty")
+	}
+
+}
+
+// test reading of dummy config
+//
+func TestConfig(t *testing.T) {
+	config := new(Config)
+	err := config.ReadConfig("test_data/sample-config.json")
+
+	if err != nil {
+		t.Error("Error while reading test config")
+	}
+
+	testConfig := new(Config)
+	testConfig.BaseUrl = "http://localhost"
+	testConfig.Port = ":8080"
+
+	if !reflect.DeepEqual(config, testConfig) {
+		err = structDiff(config, testConfig)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+}
